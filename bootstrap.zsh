9e1a59c9e3343b9df9b5c0ea4e0dc57c95ee10b9
Evan Sosenko <razorx@evansosenko.com>
2015-03-22 16:19:40 -0700

    Add archutil.

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -5,3 +5,22 @@ set -e
 function puts () {
   echo "\n-- [$1] $2"
 }
+
+function pacin () {
+  if ! (pacman -Q $1 >/dev/null); then
+    pacman -S --noconfirm $1
+  fi
+}
+
+pacin curl
+pacin python
+pacin python-yaml
+
+if ! [[ -e /usr/local/bin/archutil ]]; then
+  curl -L -o /usr/local/bin/archutil https://io.evansosenko.com/archutil/archutil
+fi
+
+if [[ -e /usr/local/bin/archutil ]]; then
+  chmod +x /usr/local/bin/archutil
+  puts 'Installed' 'archutil'
+fi
