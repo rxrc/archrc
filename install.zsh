287e166418feef87094639d76e4568576dbe6f1a
Evan Sosenko <razorx@evansosenko.com>
2015-09-08 17:59:07 -0700

    Use rbenv which in install.zsh.

--- install.zsh
+++ install.zsh
@@ -13,10 +13,10 @@ echo "\n$ bower ${cmd}\n"
 bower $cmd
 
 echo "\n$ bundle ${cmd}\n"
-bundle $cmd
+$(rbenv which bundle) $cmd
 
 echo '\n$ curate -v\n'
-sudo bundle exec curate -v
+sudo $(rbenv which bundle) exec curate -v
 
 echo '\n$ ./units.zsh\n'
 ./units.zsh
