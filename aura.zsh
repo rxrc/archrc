48ce7772ca6f984709323823f7d4ef70c6664235
Evan Sosenko <razorx@evansosenko.com>
2017-01-01 15:57:37 -0800

    Simplify aura.zsh

--- aura.zsh
+++ aura.zsh
@@ -3,43 +3,35 @@
 set -e
 set -u
 
-url='https://aur.archlinux.org/cgit/aur.git/snapshot/aura-bin.tar.gz'
-tar_file='aura-bin.tar.gz'
+function main () {
+  url='https://aur.archlinux.org/cgit/aur.git/snapshot/aura-bin.tar.gz'
+  tar_file='aura-bin.tar.gz'
 
-function pacin () {
-  if ! (pacman -Q $1 >/dev/null); then
-    sudo -S pacman -S --noconfirm $1
-  fi
+  sudo -S pacman -S --noconfirm curl grep sed sudo tar
+
+  build_dir=$(mktemp -d)
+  cd $build_dir
+
+  curl -L -o $tar_file $url
+  tar -xzf $tar_file
+  cd aura-bin
+
+  depends=$(grep "^depends=" PKGBUILD)
+  depends=("${(s/=/)depends}")
+  depends=($=depends[2])
+  depends=$(echo $depends | tr '(' ' ' | tr ')' ' ' | sed "s/'//g")
+
+  sudo -S pacman -S --noconfirm $(echo $depends)
+
+  sudo -S chgrp -R nobody $build_dir
+  sudo -S chmod -R g+rwX $build_dir
+  sudo -S -u nobody makepkg
+
+  aura_bin=$(ls aura-bin-*.xz)
+  sudo -S pacman -U --noconfirm $aura_bin
+
+  sudo -S rm -rf $build_dir
 }
 
-pacin curl
-pacin grep
-pacin tar
-pacin sed
-pacin sudo
-
-build_dir=$(mktemp -d)
-cd $build_dir
-
-curl -L -o $tar_file $url
-tar -xzf $tar_file
-cd aura-bin
-
-string=$(grep "^depends=" PKGBUILD)
-array=("${(s/=/)string}")
-depends=($=array[2])
-
-for p in $depends; do
-  pacin $(echo $p | sed 's/\((\|)\)//' | sed "s/'//g")
-done
-
-sudo -S chgrp -R nobody $build_dir
-sudo -S chmod -R g+rwX $build_dir
-sudo -S -u nobody makepkg
-
-aura_bin=$(ls aura-bin-*.xz)
-sudo -S pacman -U --noconfirm $aura_bin
-
-sudo -S rm -rf $build_dir
-
+main
 exit
