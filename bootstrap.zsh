9c4af7f46aec0fa4d42cf1d756c89b39da0c4f83
Evan Sosenko <razorx@evansosenko.com>
2017-01-01 16:12:16 -0800

    Simplify bootstrap.zsh

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -3,86 +3,71 @@
 set -e
 set -u
 
-curate_str="puts Gem.bin_path('config_curator', 'curate')"
-hostname="${1:-}"
-
 function puts () {
-  echo "\n-- [$1] $2"
+  echo "\n-- [${1:-}] ${2:-}"
 }
 
-function pacin () {
-  if ! (pacman -Q $1 >/dev/null); then
-    pacman -S --noconfirm $1
+function set_hostname () {
+  hostname="${1:-}"
+
+  if [[ -z ${hostname} ]]; then
+    echo 'Must give hostname as first argument.'
+    exit 2
+  fi
+
+  puts 'Setting' 'Hostname'
+  echo $hostname > /etc/hostname
+  hostname $hostname
+  puts 'Hostname ' $hostname
+}
+
+function install_node_modules () {
+  puts 'Installing' 'Node modules'
+  pacman -S --noconfirm npm
+  npm install yarn
+  ./node_modules/.bin/yarn
+  puts 'Installed' 'Node modules'
+
+  puts 'Installing' 'Config'
+  ./node_modules/.bin/gulp
+  puts 'Installed' 'Config'
+}
+
+function install_archutil () {
+  puts 'Installing' 'archutil requirements'
+  pacman -S --noconfirm curl python python-yaml
+  puts 'Installed' 'archutil requirements'
+
+  if ! [[ -e /usr/local/bin/archutil ]]; then
+    puts 'Installing' 'archutil'
+    curl -L -o /usr/local/bin/archutil https://io.evansosenko.com/archutil/archutil
+  fi
+
+  if [[ -e /usr/local/bin/archutil ]]; then
+    chmod +x /usr/local/bin/archutil
+    puts 'Installed' 'archutil'
   fi
 }
 
-if [[ -z ${hostname} ]]; then
-  echo 'Must give hostname as first argument.'
-  exit 1
-fi
+function set_locale () {
+  puts 'Setting' 'Locale'
+  locale-gen
+  export $(cat /etc/locale.conf)
+  puts 'Set' 'Locale'
+}
 
-if [[ $(id -u) -ne 0 ]]; then
-  echo 'Must run as root.'
-  exit 1
-fi
+function main () {
+  if [[ $(id -u) -ne 0 ]]; then
+    echo 'Must run as root.'
+    exit 1
+  fi
 
-echo $hostname > /etc/hostname
-hostname $hostname
-puts 'Hostname' $hostname
+  set_hostname ${1:-}
+  install_config
+  install_archutil
+  set_locale
+  puts 'Done' ''
+}
 
-puts 'Installing' 'Config Curator requirements'
-
-pacin ruby
-pacin yarn
-
-ruby -e $curate_str >/dev/null 2>&1 || gem install --no-document config_curator
-
-ruby -e $curate_str >/dev/null 2>&1 && \
-  puts 'Installed' 'Config Curator requirements'
-
-puts 'Installing' 'Node modules'
-yarn
-puts 'Installed' 'Node modules'
-
-puts 'Installing' 'Config'
-
-$(ruby -e $curate_str) -v
-
-if [[ -d node_modules ]]; then
-  rm -rf node_modules
-  puts 'Cleaned' 'Node modules'
-fi
-
-if [[ -z "${SUDO_COMMAND:-}" && -d /root/.gem ]]; then
-  gem uninstall --all --force --executables
-  rm -rf /root/.gem
-  rm -rf /usr/lib/ruby/gems/*/cache/*
-  puts 'Cleaned' 'Ruby gems'
-fi
-
-puts 'Installing' 'archutil requirements'
-
-pacin curl
-pacin python
-pacin python-yaml
-
-puts 'Installed' 'archutil requirements'
-
-if ! [[ -e /usr/local/bin/archutil ]]; then
-  puts 'Installing' 'archutil'
-  curl -L -o /usr/local/bin/archutil https://io.evansosenko.com/archutil/archutil
-fi
-
-if [[ -e /usr/local/bin/archutil ]]; then
-  chmod +x /usr/local/bin/archutil
-  puts 'Installed' 'archutil'
-fi
-
-puts 'Setting' 'Locale'
-
-locale-gen
-export $(cat /etc/locale.conf)
-
-puts 'Done' ''
-
+main ${1:-}
 exit
