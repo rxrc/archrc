7266bb7892c938a9f1cde06945502ce0b04bae32
Evan Sosenko <razorx@evansosenko.com>
2017-02-01 13:06:09 -0800

    Add netctl-auto-resume

--- units.zsh
+++ units.zsh
@@ -51,8 +51,11 @@ else
     sudo -S touch /etc/resolv.conf
   fi
 
-  enabled+=("netctl-auto@$(ls /sys/class/net | grep ^wl | head -1)")
-  enabled+=("netctl-auto-resume@$(ls /sys/class/net | grep ^wl | head -1)")
+  enabled+=("netctl-ifplugd@$(ls /sys/class/net | grep ^e | head -1)")
+  enabled+=("netctl-auto@$(ls /sys/class/net | grep ^w | head -1)")
+
+  enabled+=("netctl-auto-resume@$(ls /sys/class/net | grep ^w | head -1)")
+  enabled+=("netctl-auto-resume@$(ls /sys/class/net | grep ^e | head -1)")
 fi
 
 if [[ -e /etc/ddclient/ddclient.conf ]]; then
